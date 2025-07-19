66ed5fbc4bf02f625ef296c1e9bf69cbda225bed
shd <masterseo01@naver.com>
2025-07-19 21:23:06 +0900

    [mod]코드 구조 수정
     enemySpawnPoint제어를 위한 cnt 카운트 수정,
     Null예외 처리 추가,
     미사용 namespace 제거,
    
    
    Former-commit-id: b0306341e69eae0ed047d41293060e1d7c0ce114

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,79 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.Experimental.UIElements;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.SocialPlatforms;
-using static UnityEngine.UI.GridLayoutGroup;
 
+
+//맵 타일 생성 위치 및 회전 정보 struct
+public class TransformStruct
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public TransformStruct(Vector3 pos, float yRotation)
+    {
+        position = pos;
+        rotation = Quaternion.Euler(0, yRotation, 0); // Y축 오일러 각도
+    }
+}
 public class MapManager : MonoBehaviour
 {
     public static MapManager Instance { get; private set; }
+
     //맵 크기
     private int mapSize = 5;
     private float tileSize = 12;
-    //맵 타일 관리 좌표
-    private Vector3 hiddenPosition = new Vector3(-9999, -9999, -9999);
-    //맵 타일 addressable 주소
-    List<string> tileKeys = new List<string>()
+    //맵 프리팹 관리
+    private Dictionary<string, GameObject> prefabCache = new Dictionary<string, GameObject>();
+
+    private List<string> tileKeys = new List<string>()
     {
         "map_tile_01",
         "map_tile_02",
         "map_tile_03",
         "map_tile_04"
     };
-    List<string> prefabKeys = new List<string>()
+    private List<string> prefabKeys = new List<string>()
     {
         "map_corner",
         "map_wall",
         "map_door",
         "DroneSpawnPoint"
     };
-    Dictionary<string, GameObject> prefabCache = new Dictionary<string, GameObject>();
-    //맵 타일 풀링 관리
-    private Dictionary<string, List<GameObject>> mapTilePool = new Dictionary<string, List<GameObject>>();
-    //맵 타일 생성 방향
-    private enum Direction { TopLeft, TopRight, BottomLeft, BottomRight, Up, Down, Left, Right, Center }
-    //맵 타일 생성 위치 및 회전 정보 struct
-    public class TransformStruct
-    {
-        public Vector3 position;
-        public Quaternion rotation;
 
-        public TransformStruct(Vector3 pos, float yRotation)
-        {
-            position = pos;
-            rotation = Quaternion.Euler(0, yRotation, 0); // Y축 오일러 각도
-        }
-    }
-    //타일 생성 위치 및 회전 정보
-    private readonly Dictionary<Direction, TransformStruct> tileTransform = new Dictionary<Direction, TransformStruct>()
-    {
-        { Direction.TopLeft, new TransformStruct(new Vector3(-1, 0, 1), -90f)},
-        { Direction.TopRight, new TransformStruct(new Vector3(1, 0, 1),0f) },
-        { Direction.BottomLeft, new TransformStruct(new Vector3(-1, 0, -1),-180f)},
-        { Direction.BottomRight, new TransformStruct(new Vector3(1, 0, -1),90f) },
-
-        { Direction.Up, new TransformStruct(new Vector3(0, 0, 1), 0f) }, // Up
-        { Direction.Down, new TransformStruct(new Vector3(0, 0, -1), -180f) }, // Down
-        { Direction.Left, new TransformStruct(new Vector3(-1, 0, 0), -90f) }, // Left
-        { Direction.Right, new TransformStruct(new Vector3(1, 0, 0), 90f) }, // Right
-        { Direction.Center, new TransformStruct(Vector3.zero, 0f) } // Center
-    };
-
-    private GameObject[] droneSpawnPoint = new GameObject[4];
-    private GameObject map;
-    private Vector3[] mapInnerTransform;
-
-    private void Awake()
-    {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
-    }
-    public IEnumerator PreloadPrefabs(System.Action onComplete = null)
+    public IEnumerator PreloadMapPrefabs(System.Action onComplete = null)
     {
         prefabKeys.AddRange(tileKeys);
 
@@ -94,34 +63,66 @@ public class MapManager : MonoBehaviour
 
         onComplete?.Invoke(); // 프리팹 로딩 완료 시 콜백
     }
-    void OnPrefabLoaded(AsyncOperationHandle<GameObject> handle)
+
+    //맵 타일 생성 방향
+    private enum Direction { TopLeft, TopRight, BottomLeft, BottomRight, Up, Down, Left, Right, Center }
+
+    //타일 생성 위치 및 회전 정보
+    private readonly Dictionary<Direction, TransformStruct> tileTransform = new Dictionary<Direction, TransformStruct>()
     {
-        if (handle.Status != AsyncOperationStatus.Succeeded)
-        {
-            Debug.LogError("Room prefab failed to load: " + handle.Status);
-        }
+        { Direction.TopLeft, new TransformStruct(new Vector3(-1, 0, 1), -90f)},
+        { Direction.TopRight, new TransformStruct(new Vector3(1, 0, 1),0f) },
+        { Direction.BottomLeft, new TransformStruct(new Vector3(-1, 0, -1),-180f)},
+        { Direction.BottomRight, new TransformStruct(new Vector3(1, 0, -1),90f) },
+
+        { Direction.Up, new TransformStruct(new Vector3(0, 0, 1), 0f) }, // Up
+        { Direction.Down, new TransformStruct(new Vector3(0, 0, -1), -180f) }, // Down
+        { Direction.Left, new TransformStruct(new Vector3(-1, 0, 0), -90f) }, // Left
+        { Direction.Right, new TransformStruct(new Vector3(1, 0, 0), 90f) }, // Right
+        { Direction.Center, new TransformStruct(Vector3.zero, 0f) } // Center
+    };
+
+    //맵 타일 풀링 관리
+    private Dictionary<string, List<GameObject>> mapTilePool = new Dictionary<string, List<GameObject>>();
+
+    public GameObject[] enemySpawnPoint = new GameObject[4];
+
+    private GameObject map;
+
+    private Vector3[] mapInnerTransform;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
-    private void SetInstance(GameObject instance, Vector3 localPoistion, Quaternion localRotation, Transform parent, string nameSuffix = "", string poolName=null)
+
+    private void SetInstance(GameObject instance, Vector3 localPoistion, Quaternion localRotation, Transform parent, string nameSuffix = "", string poolName = null)
     {
         instance.transform.SetParent(parent, false);
         instance.transform.localPosition = localPoistion;
         instance.transform.localRotation = localRotation;
-        
+
         instance.name = instance.name.Replace("(Clone)", nameSuffix);
-        if (poolName!=null)
+        if (poolName != null)
         {
             mapTilePool[poolName].Add(instance);
         }
     }
-
+    //외부 스크립트용 getter,setter
     public void SetMapConfig(int mapSize, float tileSize)
     {
         this.mapSize = mapSize;
         this.tileSize = tileSize;
     }
-    public GameObject[] GetDroneSpawnPoint()
+    public GameObject[] GetEnemySpawnPoint()
     {
-        return droneSpawnPoint;
+        return enemySpawnPoint;
+    }
+    public Vector3 GetPlayerStartPosition()
+    {
+        if (mapInnerTransform!=null) return mapInnerTransform[ Random.Range(0, mapInnerTransform.Length)];
+        else return Vector3.zero;
     }
     //맵 초기 생성
     public void GenerateMap()
@@ -140,9 +141,9 @@ public class MapManager : MonoBehaviour
             SetInstance(Instantiate(prefabCache["map_corner"]), data.position * offset, data.rotation, map.transform, $"_{corner}");
         }
         //벽 타일 생성
+        int cnt = 0;
         for (int i = 4; i < 8; i++)
         {
-            int cnt = 0;
             Direction side = (Direction)i;
             var data = tileTransform[side];
             GameObject sideWall = new GameObject("side_wall_" + side);
@@ -161,8 +162,11 @@ public class MapManager : MonoBehaviour
                 {
                     GameObject SpawnPointInstance = Instantiate(prefabCache["DroneSpawnPoint"]);
                     SetInstance(SpawnPointInstance, Vector3.zero + Vector3.up * 3.65f, Quaternion.Euler(0,180f,0), side_Map_Tile.transform, $"_{side}");
-                    droneSpawnPoint[cnt] = SpawnPointInstance;
-
+                    if (cnt < enemySpawnPoint.Length)
+                    {
+                        enemySpawnPoint[cnt] = SpawnPointInstance;
+                        cnt++;
+                    }
                 }
             }
             //벽 타일 위치 지정
@@ -196,12 +200,12 @@ public class MapManager : MonoBehaviour
         //맵 초기화
         foreach (var mapTileAddr in mapTilePool)
         {
-            foreach(var mapTile in mapTileAddr.Value)
+            foreach (var mapTile in mapTileAddr.Value)
             {
                 mapTile.SetActive(false);
             }
         }
-        for (int i=0;i< (mapSize - 2) * (mapSize - 2); i++)
+        for (int i = 0; i < (mapSize - 2) * (mapSize - 2); i++)
         {
             string tileAdrr = tileKeys[Random.Range(0, tileKeys.Count)];
             if (!mapTilePool.ContainsKey(tileAdrr))
@@ -221,39 +225,5 @@ public class MapManager : MonoBehaviour
             }
         }
     }
-    
 
-    ////드론 스폰지점 생성 타일일때 생성
-    //if (enemySpawnOn)
-    //{
-    //    //스폰지점 관리를 위한 배열화
-    //    enemySpawns[enemySpawnCnt] = Instantiate(enemySpawn, new Vector3(0, 0, 0), Quaternion.identity);
-    //    //현재 타일 아래로 귀속
-    //    enemySpawns[enemySpawnCnt].transform.parent = maps[i, j].transform;
-    //    //위치 조정
-    //    enemySpawns[enemySpawnCnt].transform.localPosition = new Vector3(0, 3.65f, 0);
-    //    //스폰지점 갯수(인덱스) 관리
-    //    enemySpawnCnt++;
-    //    enemySpawnOn = false;
-    //}
-    //            }
-    //        }//플레이어 위치 지정을 위한 랜덤 좌표
-    //        parentI = Random.Range(1, 4);
-    //parentJ = Random.Range(1, 4);
-    ////플레이어 위치 지정 후 해당 타일을 부모로 설정
-    //this.transform.parent = maps[parentI, parentJ].transform;
-    ////위치 조정
-    //this.transform.localPosition = new Vector3(0, 0, 0);
-    ////플레이어 위치 조정
-    //player.transform.parent = this.transform;
-    //player.transform.localPosition = new Vector3(0, 15, 0);
-    //player.transform.parent = null;
-    ////플레이어 회전 축 고정(타격으로 인한 회전 방지)
-    //player_rig.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    ////스킬 카드 소환
-    //makeSkill();
-    ////물리엔진 영향으로부터 제외
-    //player_rig.isKinematic = true;
-
-    //    }
 }
