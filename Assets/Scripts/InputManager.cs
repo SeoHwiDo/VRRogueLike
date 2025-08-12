5ed344ca4234c6362906b09813aad8094d501ebe
shd <masterseo01@naver.com>
2025-08-12 22:26:54 +0900

    [mod] 메소드명칭 수정, 마우스 민감도 수정, 인스턴스화, 사격용 인풋 추가
    Former-commit-id: 30275bc87d5e2e2829a1d33a5b13812300093670

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,37 +6,44 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance {  get; private set; }
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
     public enum InputType { Mouse, Gamepad, Touch }
     public InputType inputType = InputType.Mouse;
 
-    public float mouseSensitivity = 1.0f;
+    public float mouseSensitivity = 100f;
     public float gamepadSensitivity = 100f;
     public float touchSensitivity = 0.1f;
 
     private Vector2 lastTouchPos;
     private bool isTouching;
 
-    public Vector2 GetLookDelta()
+    public Vector2 GetLookAxis()
     {
         switch (inputType)
         {
             case InputType.Mouse:
-                return GetMouseDelta() * mouseSensitivity;
+                return GetMouseAxis() * mouseSensitivity;
             case InputType.Gamepad:
-                return GetGamepadDelta() * gamepadSensitivity;
+                return GetGamepadAxis() * gamepadSensitivity;
             case InputType.Touch:
-                return GetTouchDelta() * touchSensitivity;
+                return GetTouchAxis() * touchSensitivity;
             default:
                 return Vector2.zero;
         }
     }
 
-    private Vector2 GetMouseDelta()
+    private Vector2 GetMouseAxis()
     {
         return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
     }
 
-    private Vector2 GetGamepadDelta()
+    private Vector2 GetGamepadAxis()
     {
 #if ENABLE_INPUT_SYSTEM
         if (Gamepad.current != null)
@@ -45,7 +52,7 @@ public class InputManager : MonoBehaviour
         return Vector2.zero;
     }
 
-    private Vector2 GetTouchDelta()
+    private Vector2 GetTouchAxis()
     {
         if (Input.touchCount == 1)
         {
@@ -65,5 +72,58 @@ public class InputManager : MonoBehaviour
             }
         }
         return Vector2.zero;
+    }
+    private bool GetFireKey()
+    {
+        switch (inputType)
+        {
+            case InputType.Mouse:
+                return GetMouseFireKey();
+            case InputType.Gamepad:
+                return GetGamepadFireKey();
+            case InputType.Touch:
+                return GetTouchFireKey();
+            default:
+                return false;
+        }
+    }
+    private bool GetMouseFireKey()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+    private bool GetGamepadFireKey()
+    {
+#if ENABLE_INPUT_SYSTEM
+        if (Gamepad.current != null)
+        {
+           return Gamepad.current.leftTrigger.wasPressedThisFrame;
+        }
+#endif
+        return false;
+    }
+    private bool GetTouchFireKey()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // 터치 시작 → 게이지 충전 시작
+            if (touch.phase == TouchPhase.Began)
+            {
+                // TODO: 게이지 충전 시작
+            }
+            // 터치 유지 중 → 게이지 증가
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                // TODO: 게이지 충전
+            }
+            // 터치 끝날 때 발사
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                // TODO: 게이지 충전 종료
+                return true;
+            }
+        }
+        return false;
     }
 }
