552dc2cbb4b9162528a52a25c61705e1d61aa5e6
shd <masterseo01@naver.com>
2025-08-14 23:29:55 +0900

    [mod] 사격 파티클 종료 감지시 자동으로 비활성화 후 pool에 추가
    Former-commit-id: f44f1725b99ed6bb5a7aeb8eb54e50f221403162

---
+++ Assets/Scripts/FirePtc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePtc : MonoBehaviour
+{
+    private void OnParticleSystemStopped()
+    {
+        Debug.Log("EndPtc");
+        this.gameObject.SetActive(false);
+        SkillManager.Instance.InPool(this.gameObject);
+    }
+}
