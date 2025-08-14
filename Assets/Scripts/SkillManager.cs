188c9385674e83a78290ba5f6471cb222425de45
shd <masterseo01@naver.com>
2025-08-14 23:31:29 +0900

    [mod] 사격 로직 및 관련 오브젝트 자원관리 로직 추가
    Former-commit-id: a4663a8efa742c1160645bf1e3aabc901d5ee744

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SkillManager Instance {  get; private set; }
+    private AudioSource audioSource;
+    private AudioClip fireSound;
+  
+    [SerializeField]private GameObject Bullet;
+    [SerializeField]private GameObject FirePtc;
+    private Queue<GameObject> BulletPool = new Queue<GameObject>();
+    private Queue<GameObject> FirePtcPool = new Queue<GameObject>();
+    private void Awake()
     {
-        
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        fireSound = Resources.Load<AudioClip>("Sounds/FireSound");
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void PoolingObj(Queue<GameObject> pool,GameObject obj)
+    {
+        if (pool.Count > 0)
+        {
+            GameObject reusable = pool.Dequeue();
+            reusable.transform.position = this.transform.position;
+            reusable.transform.rotation = this.transform.rotation;
+            reusable.SetActive(true);
+        }
+        else
+        {
+            GameObject newBullet = Instantiate(obj, this.transform.position, this.transform.rotation);
+        }
+    }
+    public void InPool(GameObject obj)
+    {
+        switch (obj.tag)
+        {
+            case "bullet":
+                BulletPool.Enqueue(obj);
+                return;
+            case "ptc":
+                FirePtcPool.Enqueue(obj);
+                return;
+            default:
+                return;
+        }
+    }
+    
+    public void ShootBullet()
+    {
+        audioSource.PlayOneShot(fireSound);
+        PoolingObj(FirePtcPool, FirePtc);
+        PoolingObj(BulletPool, Bullet);
     }
 }
